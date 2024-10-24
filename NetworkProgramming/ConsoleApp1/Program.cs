3987595daee6e89330ed0ec3078e74b68b2feae4
Hamire <142791987+Hamire12324@users.noreply.github.com>
2024-10-25 03:17:00 +0700

    Add files via upload

---
+++ NetworkProgramming/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.Net;
+using System.Net.Mail;
+
+namespace TestClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MailAddress to = new MailAddress("minhpy2004@gmail.com");
+            MailAddress from = new MailAddress("minhpy30@gmail.com");
+
+            MailMessage email = new MailMessage(from, to);
+            email.Subject = "Testing out email sending";
+            email.Body = "hello";
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("minhpy30@gmail.com", "udgt gplx yogd bsen");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+
+            try
+            {
+                smtp.Send(email);
+                Console.WriteLine("Email sent successfully!");
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
